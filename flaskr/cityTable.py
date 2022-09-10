67c84f1ac2bace1080f0c859b31e3d1929aeae84
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2022-09-10 13:59:50 -0700

    Can search for cities

--- flaskr/cityTable.py
+++ flaskr/cityTable.py
@@ -14,8 +14,8 @@ from flaskr.db import get_db
 
 bp = Blueprint('cityTable', __name__)
 
-owm = OWM(os.environ.get("OPENWEATHER_API_KEY"))
-reg = owm.city_id_registry()
+#owm = OWM(os.environ.get("OPENWEATHER_API_KEY"))
+#reg = owm.city_id_registry()
 
 @bp.route('/')
 def index():
@@ -34,27 +34,48 @@ def search():
     if request.method == 'POST':
         error = None
         name = str(request.form['city_name']).title()
-        state = str(request.form['state']).title()
-        country = str(request.form['country']).upper()
+        # state = str(request.form['state']).title()
+        # country = str(request.form['country']).upper()
         if not name:
             error = 'Enter City name'
 
+        if error is None:
+
+            # cities is a list of tuples
+            # cities = reg.ids_for(name, country=country, state=state, matching='exact')
+            url = 'http://api.openweathermap.org/geo/1.0/direct?q={name}&limit=5&appid={key}'\
+                .format(name=name, key=os.environ.get("OPENWEATHER_API_KEY"))
+
+            r = requests.get(url)
+
+            if r.status_code!=200:
+                error = 'Request failed'
+            else:
+                cities = r.json()
+                print((cities[4])['name'])
+                data = list()
+                for city in cities:
+                    c = list()
+                    for attribute, value in city.items():
+                        if(attribute=='local_names'):
+                            continue
+                        c.append(city[attribute])
+                    data.append(c)
+                # for i in len(range(cities)):
+                #    data.append(tuple((cities[i]).name, cities[i].lat, cities[i].lon, cities[i].country, cities[i].state))
+
+                # check if in city is in database
+                # db = get_db()
+                # for i in range(len(cities)):
+                #    city = db.execute(
+                #        'SELECT * FROM owm_cities WHERE city_id = ?', ((cities[i]).,)
+                #    ).fetchone()
+                #    if city is not None:
+                #        cities.pop(i)
+                #        i -= 1
+                return render_template('cityTable/addCity.html', cities=data)
         if error is not None:
             flash(error)
-        else:
-            # cities is a list of tuples
-            cities = reg.ids_for(name, country=country, state=state, matching='exact')
-
-            #check if in city is in database
-            db = get_db()
-            for i in range(len(cities)):
-                city = db.execute(
-                    'SELECT * FROM owm_cities WHERE city_id = ?', ((cities[i])[0],)
-                ).fetchone()
-                if city is not None:
-                    cities.pop(i)
-                    i -= 1
-            return render_template('cityTable/index/addCity', cities=cities)
     return render_template('cityTable/search.html')
 
 
@@ -94,8 +115,4 @@ def add():
 @bp.route('/<int:id>/current_weather', methods=('GET', 'POST'))
 def current_weather(id):
     db = get_db()
-    coordinates = db.execute(
-        'SELECT latitude, longitude'
-        ' FROM point'
-        ' ORDER BY created ASC'
-    ).fetchall()
+    return render_template('cityTable/current_weather.html')

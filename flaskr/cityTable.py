5c191893341685ce24bb6269e0832ee37cba6b03
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2022-09-11 13:15:13 -0700

    updated the way to add cities

--- flaskr/cityTable.py
+++ flaskr/cityTable.py
@@ -24,7 +24,7 @@ def index():
     cities = db.execute(
         ' SELECT p.city_id , city_name, city_coord_long, city_coord_lat, city_country' 
         ' FROM owm_cities p'
-        ' ORDER BY city_id DESC'
+        ' ORDER BY city_name ASC'
     ).fetchall()
     return render_template('cityTable/index.html', cities=cities)
 
@@ -32,7 +32,7 @@ def index():
 @bp.route('/search', methods=('GET', 'POST'))
 def search():
     if request.method == 'POST':
-        print('call def search in post')
+        #print('call def search in post')
         error = None
         name = str(request.form['city_name']).title()
         # state = str(request.form['state']).title()
@@ -44,6 +44,8 @@ def search():
 
             # cities is a list of tuples
             # cities = reg.ids_for(name, country=country, state=state, matching='exact')
+
+            # use geocoding api to find all cities, only returns a max of 5
             url = 'http://api.openweathermap.org/geo/1.0/direct?q={name}&limit=5&appid={key}'\
                 .format(name=name, key=os.environ.get("OPENWEATHER_API_KEY"))
 
@@ -58,70 +60,76 @@ def search():
                     #pprint(d)
                     if('local_names' in d):
                         del d['local_names']
-                #return redirect(url_for('cityTable.add', cities=data))
-                return render_template('cityTable/addcity.html', cities=data)
+                return render_template('cityTable/add.html', cities=data)
         if error is not None:
-            print('error flash')
             flash(error)
-    print('call def search in get')
+    #print('call def search in get')
     return render_template('cityTable/search.html')
 
 
-@bp.route('/addcity', methods=('GET', 'POST'))
-def add():
+@bp.route('/addcity/<string:city>', methods=('GET', 'POST'))
+def add(city=None):
+    data = json.loads(city.replace("\'", "\""))
+    error = None
+    if not data:
+        error = 'city data empty'
+    name = str(data['name'])
+    country = str(data['country'])
+    latitude = data['lat']
+    longitude = data['lon']
+    if not latitude:
+        error += ' missing lat'
+    if not longitude:
+        error += ' missing long'
+    #print(error)
+    if error is None:
 
-    if request.method == 'POST':
-        print('def add called in POST')
-        citystring = request.form['city']
-        city = json.loads(citystring.replace("\'", "\""))
-        error = None
-        if not city:
-            error = 'city data empty'
-        print(city)
-        latitude = city['lat']
-        longitude = city['lon']
-        print(latitude)
-        print(longitude)
-        if not latitude:
-            error += ' missing lat'
-        if not longitude:
-            error += ' missing long'
-        print(error)
-        if error is None:
+        url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={key}" \
+            .format(lat=latitude, lon=longitude, part="hourly,minutely,daily,alerts", key=os.environ.get("OPENWEATHER_API_KEY"))
+        r = requests.get(url)
+        if r.status_code == 200:
+            data = r.json()
+            db = get_db()
+            db.execute(
+                'INSERT INTO owm_cities (city_name, city_coord_long, city_coord_lat, city_country)'
+                ' VALUES (?, ?, ?, ?)',
+                (name, longitude, latitude, country)
+            )
 
-            url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={key}"\
-                .format(lat=latitude, lon=longitude, part="hourly,minutely,daily,alerts", key=os.environ.get("OPENWEATHER_API_KEY"))
-            r = requests.get(url)
-            if r.status_code == 200:
-                data = r.json()
-                db = get_db()
-                db.execute(
-                    'INSERT INTO owm_cities (city_name, city_coord_long, city_coord_lat, city_country)'
-                    ' VALUES (?, ?, ?, ?)',
-                    (city['name'], city['lon'], city['lat'], city['country'])
-                )
+            db.commit()
+            #print('added city')
+            return redirect(url_for('cityTable.index'))
+        else:
+            print('status code not 200')
+            error = 'Request Error'
+    if error is not None:
+        #print('error flash')
+        flash(error)
+    return redirect(url_for('cityTable.search'))
 
-                #db.execute(
-                #    'INSERT INTO owm_current_weather (city_id, city_sun_rise, city_sun_set, timezone, temperature_value )'
-                #    ' VALUES ()',
-                #    ()
-                #)
-                db.commit()
-                print('added city')
-                return redirect(url_for('cityTable.index'))
-            else:
-                print('status code not 200')
-                error = 'Request Error'
-        if error is not None:
-            print('error flash')
-            flash(error)
-    print('def add called in GET')
-    #pprint(request.args['cities'])
-    #return redirect(url_for('cityTable.add', city = request.args['cities']))
-    return render_template('cityTable/index.html' )
+@bp.route('/updatecurrent/<int:city_id>', methods=('GET', 'POST'))
+def update_current_weather(city_id):
+    db = get_db()
+    city = db.execute
+    (
+        'SELECT * FROM owm_cities WHERE city_id = ?', (city_id,)
+    ).fetchone()
+    error = None
+    if city is None:
+        error = 'city not currently tracked'
+    latitude = city['city_coord_lat']
+    longitude = city['city_coord_long']
 
 
-@bp.route('/<int:id>/current_weather', methods=('GET', 'POST'))
+def update_current_weather(latitude, longitude):
+    db = get_db()
+    id = db.execute
+    (
+
+    )
+    return
+
+@bp.route('/weather/<int:id>', methods=('GET', 'POST'))
 def current_weather(id):
     db = get_db()
     return render_template('cityTable/currentweather.html')

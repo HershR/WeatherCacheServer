7a418d4c37453194a1080d3aa7814075795b8bcc
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2022-09-14 15:42:49 -0700

    added ability to search and add city by coordinates

--- flaskr/cityTable.py
+++ flaskr/cityTable.py
@@ -13,6 +13,7 @@ from pyowm.owm import OWM
 from werkzeug.exceptions import abort
 from flaskr.weather import update_current_weather
 from flaskr.db import get_db
+from flaskr.weather import update_current_weather
 
 bp = Blueprint('cityTable', __name__)
 
@@ -30,6 +31,62 @@ def index():
     ).fetchall()
     return render_template('cityTable/index.html', cities=cities)
 
+
+@bp.route('/search/coordinates', methods=('GET', 'POST'))
+def search_by_coordinates():
+    if request.method == 'POST':
+        coordinate = str(request.form['coordinate']).replace(" ","")
+        error = None
+        coord = []
+        latitude = None
+        longitude = None
+
+        if not coordinate:
+            error = 'Enter Coordinates'
+        elif ',' not in coordinate:
+            error = 'Check format'
+        else:
+            coord = coordinate.split(',')
+        if len(coord) != 2:
+            error = 'Check format'
+        else:
+            print(coord)
+            latitude = coord[0]
+            longitude = coord[1]
+            if not latitude:
+                error = 'Latitude is required.'
+
+            if not longitude:
+                error = 'Longitude is required.'
+        if error is not None:
+            flash(error)
+        else:
+
+            # cities is a list of tuples
+            # cities = reg.ids_for(name, country=country, state=state, matching='exact')
+
+            # use geocoding api to find all cities, only returns a max of 5
+            url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}".format(lat=latitude, lon=longitude, key=os.environ.get("OPENWEATHER_API_KEY"))
+            r = requests.get(url)
+
+            if r.status_code != 200:
+                error = 'Request failed'
+                print(latitude)
+                print(longitude)
+            else:
+                data = r.json()
+                db = get_db()
+                db.execute(
+                    'INSERT INTO owm_cities (city_id, city_name, city_coord_long, city_coord_lat, city_country)'
+                    ' VALUES (?, ?, ?, ?, ?)',
+                    (data['id'], data['name'], data['coord']['lon'],data['coord']['lat'], data['sys']['country'])
+                )
+                db.commit()
+                update_current_weather(data['id'])
+                return redirect(url_for('cityTable.index'))
+        if error is not None:
+            flash(error)
+    return render_template('cityTable/searchcoordinates.html')
 
 @bp.route('/search', methods=('GET', 'POST'))
 def search():

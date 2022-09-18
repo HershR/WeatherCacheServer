ce4a408e1050c83f78262e529d245d8c367bf680
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2022-09-18 12:02:27 -0700

    Fixed bug in search_by_coordinates()
    Forgot to updated key name, is fixed now

--- flaskr/cityTable.py
+++ flaskr/cityTable.py
@@ -12,9 +12,6 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 
-from pprint import pprint
-from pyowm.owm import OWM
-
 from werkzeug.exceptions import abort
 from flaskr.weather import update_current_weather
 from flaskr.db import get_db
@@ -22,25 +19,23 @@ from flaskr.weather import update_current_weather
 
 bp = Blueprint('cityTable', __name__)
 
-#owm = OWM(os.environ.get("OPENWEATHER_API_KEY"))
-#reg = owm.city_id_registry()
+key = os.environ.get("OPENWEATHERMAP_API_KEY")
 
 @bp.route('/')
 def index():
-    #print('index')
     db = get_db()
     cities = db.execute(
-        ' SELECT p.city_id , city_name, city_coord_long, city_coord_lat, city_country' 
-        ' FROM owm_cities p'
+        ' SELECT city_id , city_name, city_coord_long, city_coord_lat, city_country' 
+        ' FROM owm_cities'
         ' ORDER BY city_name ASC'
     ).fetchall()
     return render_template('cityTable/index.html', cities=cities)
 
-
+# search and add city by its coordinates
 @bp.route('/search/coordinates', methods=('GET', 'POST'))
 def search_by_coordinates():
     if request.method == 'POST':
-        coordinate = str(request.form['coordinate']).replace(" ","")
+        coordinate = str(request.form['coordinate']).replace(" ", "")
         error = None
         coord = []
         latitude = None
@@ -55,21 +50,18 @@ def search_by_coordinates():
         if len(coord) != 2:
             error = 'Check format'
         else:
-            #print(coord)
             latitude = coord[0]
             longitude = coord[1]
             if not latitude:
                 error = 'Latitude is required.'
             if not longitude:
                 error = 'Longitude is required.'
-        if error is not None:
-            flash(error)
-        else:
-            url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}".format(lat=latitude, lon=longitude, key=os.environ.get("OPENWEATHER_API_KEY"))
+        if error is None:
+            url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}".format(lat=latitude, lon=longitude, key=key)
             r = requests.get(url)
 
             if r.status_code != 200:
-                error = 'Request failed'
+                error = 'Search Error: Request failed Status code: ' +str(r.status_code)
             else:
                 data = r.json()
                 c = db.execute('SELECT * FROM owm_cities '
@@ -85,16 +77,16 @@ def search_by_coordinates():
                         (data['id'], data['name'], data['coord']['lon'], data['coord']['lat'], data['sys']['country'])
                     )
                     db.commit()
-                    update_current_weather(data['id'])
+                    #update_current_weather(data['id'])
                     return redirect(url_for('cityTable.index'))
         if error is not None:
             flash(error)
     return render_template('cityTable/searchcoordinates.html')
 
+# search city by name
 @bp.route('/search', methods=('GET', 'POST'))
 def search():
     if request.method == 'POST':
-        #print('call def search in post')
         error = None
         name = str(request.form['city_name']).title()
         # state = str(request.form['state']).title()
@@ -103,10 +95,6 @@ def search():
             error = 'Enter City name'
 
         if error is None:
-
-            # cities is a list of tuples
-            # cities = reg.ids_for(name, country=country, state=state, matching='exact')
-
             # use geocoding api to find all cities, only returns a max of 5
             url = 'http://api.openweathermap.org/geo/1.0/direct?q={name}&limit=5&appid={key}'\
                 .format(name=name, key=os.environ.get("OPENWEATHER_API_KEY"))
@@ -128,7 +116,7 @@ def search():
     #print('call def search in get')
     return render_template('cityTable/search.html')
 
-
+# add a city by name
 @bp.route('/addcity/<string:city>', methods=('GET', 'POST'))
 def add(city=None):
     data = json.loads(city.replace("\'", "\""))

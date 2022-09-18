0b3e9a78d0aba90924d2c7866a28eafda70c3ab4
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2022-09-18 12:11:00 -0700

    removed ability to search and add cities by name

--- flaskr/cityTable.py
+++ flaskr/cityTable.py
@@ -83,68 +83,6 @@ def search_by_coordinates():
             flash(error)
     return render_template('cityTable/searchcoordinates.html')
 
-# search city by name
-@bp.route('/search', methods=('GET', 'POST'))
-def search():
-    if request.method == 'POST':
-        error = None
-        name = str(request.form['city_name']).title()
-        # state = str(request.form['state']).title()
-        # country = str(request.form['country']).upper()
-        if not name:
-            error = 'Enter City name'
-
-        if error is None:
-            # use geocoding api to find all cities, only returns a max of 5
-            url = 'http://api.openweathermap.org/geo/1.0/direct?q={name}&limit=5&appid={key}'\
-                .format(name=name, key=os.environ.get("OPENWEATHER_API_KEY"))
-
-            r = requests.get(url)
-
-            if r.status_code!=200:
-                error = 'Request failed'
-            else:
-                cities = r.json()
-                data = cities
-                for d in data:
-                    #pprint(d)
-                    if('local_names' in d):
-                        del d['local_names']
-                return render_template('cityTable/add.html', cities=data)
-        if error is not None:
-            flash(error)
-    #print('call def search in get')
-    return render_template('cityTable/search.html')
-
-# add a city by name
-@bp.route('/addcity/<string:city>', methods=('GET', 'POST'))
-def add(city=None):
-    data = json.loads(city.replace("\'", "\""))
-    error = None
-    if not data:
-        error = 'city data empty'
-    name = str(data['name'])
-    country = str(data['country'])
-    latitude = data['lat']
-    longitude = data['lon']
-    if not latitude:
-        error += ' missing lat'
-    if not longitude:
-        error += ' missing long'
-    if error is None:
-        db = get_db()
-        db.execute(
-            'INSERT INTO owm_cities (city_name, city_coord_long, city_coord_lat, city_country)'
-            ' VALUES (?, ?, ?, ?)',
-            (name, longitude, latitude, country)
-        )
-        db.commit()
-        return redirect(url_for('cityTable.index'))
-
-    else:
-        flash(error)
-    return redirect(url_for('cityTable.search'))
-
 
 @bp.route('/delete/<int:city_id>', methods=('GET', 'POST'))
 def delete(city_id=None):
